1aa3d25bd68c6e53ccf9a41c20c19d2f8d58ce12
sbx_user1061 <sbx_user1061@ip-10-128-250-182.eu-west-2.compute.internal>
2018-08-18 10:31:01 +0000

    Sat Aug 18 10:31:01 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -5,10 +5,15 @@ import befaster.runner.SolutionNotImplementedException;
 public class SumSolution {
 
     public int compute(int x, int y) {
-        if
+        validatArgument(x);
+        validatArgument(y);
+
+        return x + y;
     }
 
     public void validatArgument(int argument) throws IllegalArgumentException {
-        if (argument < )
+        if ((argument < 0) || (argument > 100)) {
+            throw new IllegalArgumentException("argument out of bounds");
+        }
     }
 }
